80ff688c0cbf01c302b2619e3f057b98c617c758
iKlsR <iklsr@outlook.com>
2012-08-08 20:51:13 -0500

    new function to load cubemaps and changed interpolation for a filter

--- src/obp/loader.hpp
+++ src/obp/loader.hpp
@@ -173,7 +173,7 @@ GLuint obp_load_cmap (const char * xpos, const char * xneg, const char * ypos, c
     SOIL_free_image_data(zneg_image);
     
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
